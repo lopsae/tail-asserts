f175fbb85b6e905bf3f45ceeffaa37a951fa1b85
Maic Lopez Saenz <maic@lopsae.com>
2022-05-16 09:58:36 -0700

    xcode project created

---
+++ TailAsserts/TailAsserts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TailAsserts
+//
+//  Created by Maic Lopez Saenz on 5/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
