879a780049aa29eb2966445925821fa4cce758d7
Maic Lopez Saenz <maic@lopsae.com>
2022-09-30 12:04:32 -0700

    add two more links about scenes

--- TailAsserts/AppDelegate.swift
+++ TailAsserts/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Deleting the scene configuration in `info.plist` seems to force the
     // `UISceneSession` to be recreated in every launch. Some launches also
     // seem to dispose of the older session.
+    // https://gist.github.com/HiddenJester/e5409ce2ca823b0003c59ce11a494b1d
+    // https://hacknicity.medium.com/how-to-switch-your-ios-app-and-scene-delegates-for-improved-testing-9746279378c3
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let configuration = connectingSceneSession.configuration
         configuration.delegateClass = SceneDelegate.self
